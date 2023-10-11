d45fbb47387baee802ab47eb3440c718ce723674
Chenjunjie1996 <74704889+Chenjunjie1996@users.noreply.github.com>
2023-10-11 16:39:44 +0800

    update

--- 3.Classes and Protocols/14.Inheritance, For Better or for Worse.py
+++ 3.Classes and Protocols/14.Inheritance, For Better or for Worse.py
@@ -80,3 +80,116 @@ print(d['a'])
 print(d)
 
 # Multiple Inheritance and Method Resolution Order
+"""
+diamond problem:
+naming conflicts when superclasses implement a method by the same name
+"""
+class Root:
+    def ping(self):
+        print(f'{self}.ping() in Root')
+
+    def pong(self):
+        print(f'{self}.pong() in Root')
+
+    def __repr__(self):
+        cls_name = type(self).__name__
+        return f'<instance of {cls_name}>'
+
+class A(Root):
+    """both call super()"""
+    def ping(self):
+        print(f'{self}.ping() in A')
+        super().ping()
+
+    def pong(self):
+        print(f'{self}.pong() in A')
+        super().pong()
+
+class B(Root):
+    """ping method call super()"""
+    def ping(self):
+        print(f'{self}.ping() in B')
+        super().ping()
+
+    def pong(self):
+        print(f'{self}.pong() in B')
+
+class Leaf(A, B):
+    def ping(self):
+        print(f'{self}.ping() in Leaf')
+        super().ping()
+
+leaf1 = Leaf()
+leaf1.ping()
+leaf1.pong()
+
+print(Leaf.__mro__)
+
+# the dynamic nature of super()
+class U():
+    def ping(self):
+        print(f'{self}.ping() in U')
+        super().ping()
+
+class LeafUA(U, A):
+    def ping(self):
+        print(f'{self}.ping() in LeafUA')
+        super().ping()
+
+u = U()
+# u.ping() AttributeError: 'super' object has no attribute 'ping'
+leaf2 = LeafUA()
+leaf2.ping()
+print(LeafUA.__mro__)
+
+# Mixin Classes
+"""
+A mixin class is designed to be subclassed together with at least one other class in a
+multiple inheritance arrangement. A mixin is not supposed to be the only base class
+of a concrete class, because it does not provide all the functionality for a concrete
+object, but only adds or customizes the behavior of child or sibling classes.
+"""
+
+## Case-Insensitive Mappings
+def _upper(key):
+    try:
+        return key.upper()
+    except AttributeError:
+        return key
+
+class UpperCaseMixin:
+    def __setitem__(self, key, item):
+        super().__setitem__(_upper(key), item)
+
+    def __getitem__(self, key):
+        return super().__getitem__(_upper(key))
+
+    def get(self, key, default=None):
+        return super().get(_upper(key), default)
+
+    def __contains__(self, key):
+        return super().__contains__(_upper(key))
+
+class UpperDict(UpperCaseMixin, collections.UserDict):
+    pass
+
+class UpperCounter(UpperCaseMixin, collections.Counter):
+    """Specialized 'Counter' that uppercases string keys"""
+    pass
+
+d = UpperDict([('a', 'letter A'), (2, 'digit two')])
+print(list(d.keys()))
+d['b'] = 'letter B'
+print('b' in d)
+print(d['a'], d.get('B'))
+print(list(d.keys()))
+
+c = UpperCounter('BaNanA')
+print(c.most_common())
+
+# Multiple Inheritance in the Real World
+## ABCs Are Mixins Too
+"""
+In the Python standard library, the most visible use of 
+multiple inheritance is the collections.abc package.
+"""
